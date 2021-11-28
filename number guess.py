4ce0dfa7c404e5fe1d26dc5c4085f3dae0a8b09a
Amankumar130 <72845829+Amankumar130@users.noreply.github.com>
2021-11-28 11:12:32 +0530

    Update number guess.py

--- number guess.py
+++ number guess.py
@@ -1,18 +1,11 @@
 import random
 
-top_of_range = input("Type a number: ")
+x = input("Type a number: ")
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+if x.isdigit():
+    x = int(x)
 
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
-        quit()
-else:
-    print('Please type a number next time.')
-    quit()
-
-random_number = random.randint(0, top_of_range)
+random_number = random.randint(0, x)
 guesses = 0
 
 while True:
@@ -25,7 +18,7 @@ while True:
         continue
 
     if user_guess == random_number:
-        print("congratulations you got it")
+        print("$$$  congratulations $$$")
         break
     elif user_guess > random_number:
         print("You were above the number!")
